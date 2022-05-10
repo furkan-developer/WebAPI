7270344bf74b6faf3a782bd29cfedacac3ac5c23
Furkan Aydin <furknn2112@gmail.com>
2022-05-10 21:43:15 +0300

    Cors Configuration with extention

---
+++ WebAPI.ProjectManagement/Extensions/ServiseExtesions.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI.ProjectManagement.Extensions
+{
+    public static class ServiseExtesions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                 builder
+                 .AllowAnyOrigin() //Tum originden gelen istekleri kabul eder.
+                 .AllowAnyMethod()  //Tum metotlar kabul ediliyor.
+                 .AllowAnyHeader()); //Tum HTTP isteklerinin başlıklarını kabul eder.
+            });
+        }
+
+    }
+}
