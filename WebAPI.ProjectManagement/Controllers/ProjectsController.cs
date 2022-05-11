71c2556adb8320fd16759870bd383c2ea94d8f98
Furkan Aydin <furknn2112@gmail.com>
2022-05-11 21:14:58 +0300

    Entities katmanı oluşturuldu.

---
+++ WebAPI.ProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Onion.Entities.Models;
+
+namespace Onion.WebAPI.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly List<Project> projects;
+
+        public ProjectsController()
+        {
+            projects = new List<Project>
+            {
+                new Project { Id = Guid.NewGuid(), Name = "project 1"},
+                new Project { Id = Guid.NewGuid(), Name = "project 2"}
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return  Ok(projects);
+        }
+    }
+}
