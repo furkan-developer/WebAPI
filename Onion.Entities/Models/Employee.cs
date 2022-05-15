2d402ed5c0c7b154cd7939e32d42ff653817e81b
Furkan Aydin <furknn2112@gmail.com>
2022-05-15 09:35:55 +0300

    _01_appsettings.json da connection string ile veri tabanı linki tanımlandı.

---
+++ Onion.Entities/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Entities.Models
+{
+    public class Employee
+    {
+        [Column("EmployeeId")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "FirstName is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        public string LastName { get; set; }
+
+        public int? Age { get; set; }
+        public string? Position { get; set; }
+
+        [ForeignKey(nameof(Project))]
+        public Guid? ProjectId { get; set; }  /* FK */
+        public Project Project { get; set; } /* Navigation property */
+    }
+}
